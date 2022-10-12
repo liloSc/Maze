de3d563bcba5ef0aac86a7b3b652aeebc25ff271
liloSc <52287760+liloSc@users.noreply.github.com>
2022-10-12 13:42:36 +0200

    Startpage

--- src/main/java/com/example/maze/Main.java
+++ src/main/java/com/example/maze/Main.java
@@ -8,11 +8,12 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxml_startpage = new FXMLLoader(Main.class.getResource("startpage.fxml"));
+        Scene scene = new Scene(fxml_startpage.load(), 800, 600);
+        stage.setTitle("Name of Game");
         stage.setScene(scene);
         stage.show();
     }
