454693c938f3044c4d5683c21200349e9bbc4269
lilalimpeldehseba <54209093+lilalimpeldehseba@users.noreply.github.com>
2022-10-13 15:40:28 +0200

    made MVC Folders!

---
+++ src/main/java/com/example/maze/Model/Game_layout.java
@@ -0,0 +1,17 @@
+package com.example.maze;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.scene.layout.Pane;
+
+public class Game_layout {
+
+		
+	
+	
+}
