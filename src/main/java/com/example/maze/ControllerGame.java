c269848bccfc7bc687339c8bc3996d9270ec9654
liloSc <52287760+liloSc@users.noreply.github.com>
2022-10-15 11:11:03 +0200

    Refactor Controller Game

--- src/main/java/com/example/maze/ControllerGame.java
+++ src/main/java/com/example/maze/ControllerGame.java
@@ -5,13 +5,11 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
@@ -19,8 +17,6 @@ import javafx.util.Duration;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class ControllerGame implements Initializable {
@@ -29,46 +25,33 @@ public class ControllerGame implements Initializable {
     private final Double rectangleSize = 50.;
     //The Rectangle is created, at position (250,250)
     private final Rectangle rectangle_player = new Rectangle(250, 250, rectangleSize, rectangleSize);
-    //First snake tail created behind the head of the snake
-    //  Rectangle snakeTail_1 = new Rectangle(snakeHead.getX() - snakeSize,snakeHead.getY(),snakeSize,snakeSize);
 
-    //x and y position of the snake head different from starting position
+    //x and y position of the rectangle different from starting position
     double xPos = rectangle_player.getLayoutX();
     double yPos = rectangle_player.getLayoutY();
-
-    //Direction snake is moving at start
-    private Direction direction = Direction.RIGHT;
-
-    //List of all position of thew snake head
-    private final List<Position> positions = new ArrayList<>();
+    private Direction direction;
 
 
-    //Game ticks is how many times the snake have moved
+    //Game ticks is how many times the rectangle have moved
     private int gameTicks = 0;
 
     @FXML
     private AnchorPane anchorPane;
-    @FXML
-    private Button startButton;
+    //Variable to look if key is pressed
+    boolean isActive = false;
 
-    //Timeline that is running the game every time the KeyFrame is called (0.3 s)
-    Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.3), e -> {
-        positions.add(new Position(rectangle_player.getX() + xPos, rectangle_player.getY() + yPos));
-        moveSnakeHead(rectangle_player);
 
+    //Timeline that is running the game every time the KeyFrame is called (0.1s)
+    Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0.1), e -> {
+        movePlayer(rectangle_player);
         gameTicks++;
     }));
     Image image;
+
     //Method called after the stage is loaded
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        //  snakeBody.add(player);
-        // Image img = new Image("resources/dog_left_1.png");
-        // rectangle_player.setFill(new ImagePattern(img));
-
-      image= getImage();
-
-        //  rectangle_player.setFill(Color.RED);
+        image = getImage();
         rectangle_player.setFill(new ImagePattern(image));
         timeline.setCycleCount(Animation.INDEFINITE);
         timeline.play();
@@ -77,15 +60,16 @@ public class ControllerGame implements Initializable {
 
     private Image getImage() {
         String path = "resources/player/dog_left_1.png";
-
-        if (direction == Direction.UP && isActive)
-            path = "resources/player/dog_up_1.png";
-        if (direction == Direction.DOWN && isActive)
-            path = "resources/player/dog_down_1.png";
-        if (direction == Direction.LEFT && isActive)
-            path = "resources/player/dog_left_1.png";
-        if (direction == Direction.RIGHT && isActive)
-            path = "resources/player/dog_right_1.png";
+        if (isActive) {
+            if (direction == Direction.UP)
+                path = "resources/player/dog_up_1.png";
+            if (direction == Direction.DOWN)
+                path = "resources/player/dog_down_1.png";
+            if (direction == Direction.LEFT)
+                path = "resources/player/dog_left_1.png";
+            if (direction == Direction.RIGHT)
+                path = "resources/player/dog_right_1.png";
+        }
         Image image;
         try {
             image = new Image(new FileInputStream(path));
@@ -98,10 +82,10 @@ public class ControllerGame implements Initializable {
 
     @FXML
     void start(MouseEvent event) {
-        //Restart not implemented yet
+        //TODO: Restart not implemented yet
     }
 
-    boolean isActive = false;
+
 
     //Change position with key pressed
     @FXML
@@ -122,7 +106,7 @@ public class ControllerGame implements Initializable {
                 direction = Direction.RIGHT;
                 isActive = true;
             }
-            image= getImage();
+            image = getImage();
             rectangle_player.setFill(new ImagePattern(image));
         }
     }
@@ -135,21 +119,20 @@ public class ControllerGame implements Initializable {
         }
     }
 
-
-    //Snake head is moved in the direction specified
-    private void moveSnakeHead(Rectangle snakeHead) {
+    //Player is moved in the direction specified
+    private void movePlayer(Rectangle player) {
         if (direction.equals(Direction.RIGHT) && isActive) {
             xPos = xPos + rectangleSize;
-            snakeHead.setTranslateX(xPos);
+            player.setTranslateX(xPos);
         } else if (direction.equals(Direction.LEFT) && isActive) {
             xPos = xPos - rectangleSize;
-            snakeHead.setTranslateX(xPos);
+            player.setTranslateX(xPos);
         } else if (direction.equals(Direction.UP) && isActive) {
             yPos = yPos - rectangleSize;
-            snakeHead.setTranslateY(yPos);
+            player.setTranslateY(yPos);
         } else if (direction.equals(Direction.DOWN) && isActive) {
             yPos = yPos + rectangleSize;
-            snakeHead.setTranslateY(yPos);
+            player.setTranslateY(yPos);
         }
     }
 }
