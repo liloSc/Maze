0902ff993b08e93304e3ec0142088ac0bb99bef3
lilalimpeldehseba <54209093+lilalimpeldehseba@users.noreply.github.com>
2022-10-22 14:00:08 +0200

    Gridpane added in gamelayout

--- src/main/java/com/example/maze/Main.java
+++ src/main/java/com/example/maze/Main.java
@@ -7,27 +7,33 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
+			
+	public ControllerGame_layout gamecontroller;
+	
+	
     @Override
     public void start(Stage stage) {
         try {
-            Parent startpage = FXMLLoader.load(getClass().getResource("startpage.fxml"));
+            Parent startpage = FXMLLoader.load(getClass().getResource("Game_layout.fxml"));
             Scene scene = new Scene(startpage);
             stage.setScene(scene);
             stage.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
+       // gamecontroller = new ControllerGame_layout();
+       // gamecontroller.layoutcontroller();
+    //    gamecontroller.Main = this;
     }
 
-  /*  public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("game.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
-    }*/
+    
+    
+    
+    
+    
 
     public static void main(String[] args) {
+    	
         launch();
     }
 }
