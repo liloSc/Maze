4864b45734e29dbeb08fc9682e38666effa1760c
liloSc <52287760+liloSc@users.noreply.github.com>
2022-10-23 22:20:32 +0200

    Change moving method

--- src/main/java/com/example/maze/CombiningGame.java
+++ src/main/java/com/example/maze/CombiningGame.java
@@ -31,7 +31,7 @@ public class CombiningGame implements Initializable {
 
     @FXML
     private GridPane gamelayoutgrid;
-  
+
 
     // private FlowPane flowpane;
 
@@ -51,17 +51,17 @@ public class CombiningGame implements Initializable {
 
     //Variable to look if key is pressed
     boolean isActive = false;
-    
-    
+
+
     Game_layout gameLayout;
     private int[][] grid;
-	private int length = 61;
-	private int height = 41;
-	int i, j;
-	Image path = new Image("file:resources/player/GridB1.png",20,20, false, false);
-	Image wall = new Image("file:resources/player/GridW1.png",20,20, false, false);
-	
-	ImageView backgroundView;
+    private int length = 61;
+    private int height = 41;
+    int i, j;
+    Image path = new Image("file:resources/player/GridB1.png", 20, 20, false, false);
+    Image wall = new Image("file:resources/player/GridW1.png", 20, 20, false, false);
+
+    ImageView backgroundView;
 
 
     //Timeline that is running the game every time the KeyFrame is called (0.1s)
@@ -80,11 +80,11 @@ public class CombiningGame implements Initializable {
     //Method called after the stage is loaded
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-    	
+
         image = getCharacterImage();
         rectangleid.setFill(new ImagePattern(image));
-        
-	    
+
+
         doorClose.getLayoutBounds();
         xPosDoor = doorClose.getLayoutX();
         yPosDoor = doorClose.getLayoutY();
@@ -102,19 +102,19 @@ public class CombiningGame implements Initializable {
         String path = "resources/player/dog_left_1.png";
 
         if (isActive) {
-       	 
-                if (direction == Direction.UP)
-                    path = "resources/player/dog_up_1.png";
-                if (direction == Direction.DOWN)
-                    path = "resources/player/dog_down_1.png";
-                if (direction == Direction.LEFT)
-                    path = "resources/player/dog_left_1.png";
-                if (direction == Direction.RIGHT)
-                    path = "resources/player/dog_right_1.png";
+
+            if (direction == Direction.UP)
+                path = "resources/player/dog_up_1.png";
+            if (direction == Direction.DOWN)
+                path = "resources/player/dog_down_1.png";
+            if (direction == Direction.LEFT)
+                path = "resources/player/dog_left_1.png";
+            if (direction == Direction.RIGHT)
+                path = "resources/player/dog_right_1.png";
 
         }
-        
- 
+
+
         //   Image image;
         try {
             image = new Image(new FileInputStream(path));
@@ -124,46 +124,43 @@ public class CombiningGame implements Initializable {
         return image;
     }
 
-    
-	//fill the grid with a simple background
-	private Node printGrid() {
-		grid = new int [length][height];
-		gameLayout = new Game_layout(grid);
 
-		
+    //fill the grid with a simple background
+    private Node printGrid() {
+        grid = new int[length][height];
+        gameLayout = new Game_layout(grid);
 
-		GridPane GPane = new GridPane();
-		i = 0;
-		j = 0;
-		for(i = 0; i<length-1; i++) {
-			for (j = 0; j<height-1; j++) {
-				
-				if (gameLayout.isWall(i, j) == true) {
-	
-				backgroundView = new ImageView();
-				backgroundView.setImage(wall);
-				
-				
-				//System.out.println(grid[i][j]);
-				}
-				
-				else {
-					backgroundView = new ImageView();
-					backgroundView.setImage(path);
-					
-					
-					//System.out.println(grid[i][j]);
-				}
-				
-				gamelayoutgrid.add(backgroundView, i, j);
+
+        GridPane GPane = new GridPane();
+        i = 0;
+        j = 0;
+        for (i = 0; i < length - 1; i++) {
+            for (j = 0; j < height - 1; j++) {
+
+                if (gameLayout.isWall(i, j) == true) {
+
+                    backgroundView = new ImageView();
+                    backgroundView.setImage(wall);
+
+
+                    //System.out.println(grid[i][j]);
+                } else {
+                    backgroundView = new ImageView();
+                    backgroundView.setImage(path);
+
+
+                    //System.out.println(grid[i][j]);
+                }
+
+                gamelayoutgrid.add(backgroundView, i, j);
                 backgroundView.toBack();
-			}
-			
-		}
-		
-		return GPane;
-	}
-	
+            }
+
+        }
+
+        return GPane;
+    }
+
     @FXML
     void start(MouseEvent event) {
         //TODO: Restart not implemented yet
@@ -192,6 +189,7 @@ public class CombiningGame implements Initializable {
             }
             image = getCharacterImage();
             rectangleid.setFill(new ImagePattern(image));
+            rectangleid.toFront();
         }
     }
 
@@ -204,21 +202,26 @@ public class CombiningGame implements Initializable {
         }
     }
 
-   
+    int rowIndex;
+    int columnIndex;
+
     private void moveRectangle(Rectangle rectangle) {
+
         if (direction.equals(Direction.RIGHT) && isActive) {
-            xPos = xPos + rectangleSize;
-            rectangle.setTranslateX(xPos);
+            gamelayoutgrid.setColumnIndex(rectangleid, GridPane.getColumnIndex(rectangleid) + 1);
+
         } else if (direction.equals(Direction.LEFT) && isActive) {
-            xPos = xPos - rectangleSize;
-            rectangle.setTranslateX(xPos);
+            gamelayoutgrid.setColumnIndex(rectangleid, GridPane.getColumnIndex(rectangleid) - 1);
+
         } else if (direction.equals(Direction.UP) && isActive) {
-            yPos = yPos - rectangleSize;
-            rectangle.setTranslateY(yPos);
+            gamelayoutgrid.setRowIndex(rectangleid, GridPane.getRowIndex(rectangleid) - 1);
+
         } else if (direction.equals(Direction.DOWN) && isActive) {
-            yPos = yPos + rectangleSize;
-            rectangle.setTranslateY(yPos);
+            gamelayoutgrid.setRowIndex(rectangleid, GridPane.getRowIndex(rectangleid) + 1);
+
         }
+  
+
         //   System.out.println("[" + xPos + ", " + yPos + "]");
     }
 
@@ -242,11 +245,12 @@ public class CombiningGame implements Initializable {
             return false;
         }
     }
+
     public void initData2(Player player) { // THIS METHOD accepts a player to initialize the view
-    	gamePlayer = player;
-    	System.out.println("3 Now we have a new char in game " + gamePlayer);
+        gamePlayer = player;
+        System.out.println("3 Now we have a new char in game " + gamePlayer);
     }
-   
+
 }
 
 
