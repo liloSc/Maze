3245b57d235889c1a9dd52bda47efadc16c0b6dc
Noémie Tapie <noemie.tapie@gmail.com>
2022-10-23 20:38:04 +0200

    Wall finished

--- src/main/java/com/example/maze/Game_layout.java
+++ src/main/java/com/example/maze/Game_layout.java
@@ -190,8 +190,208 @@ public class Game_layout {
 	    		}
 	    	}
 	    	
+	    	for (i = 0; i < 17; i++ ) {
+	    		for (j=15; j<17; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 21; i < 25; i++ ) {
+	    		for (j=15; j<17; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
 	    	
-	    	
+	    	for (i = 29; i < 45; i++ ) {
+	    		for (j=15; j<17; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 47; i < 57; i++ ) {
+	    		for (j=15; j<17; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 15; i < 17; i++ ) {
+	    		for (j=17; j<19; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 39; i < 41; i++ ) {
+	    		for (j=17; j<19; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 3; i < 17; i++ ) {
+	    		for (j=19; j<21; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 3; i < 17; i++ ) {
+	    		for (j=19; j<21; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 23; i < 25; i++ ) {
+	    		for (j=19; j<29; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 39; i < 59; i++ ) {
+	    		for (j=19; j<21; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 31; i < 37; i++ ) {
+	    		for (j=19; j<21; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 35; i < 37; i++ ) {
+	    		for (j=21; j<23; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 3; i < 19; i++ ) {
+	    		for (j=23; j<25; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 25; i < 33; i++ ) {
+	    		for (j=23; j<25; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 35; i < 57; i++ ) {
+	    		for (j=23; j<25; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 3; i < 5; i++ ) {
+	    		for (j=25; j<27; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 15; i < 17; i++ ) {
+	    		for (j=25; j<37; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 47; i < 49; i++ ) {
+	    		for (j=25; j<27; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 3; i < 11; i++ ) {
+	    		for (j=27; j<29; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 19; i < 23; i++ ) {
+	    		for (j=27; j<29; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 27; i < 49; i++ ) {
+	    		for (j=27; j<29; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 51; i < 57; i++ ) {
+	    		for (j=27; j<29; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 27; i < 29; i++ ) {
+	    		for (j=29; j<31; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 39; i < 41; i++ ) {
+	    		for (j=29; j<31; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 51; i < 53; i++ ) {
+	    		for (j=29; j<31; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 1; i < 13; i++ ) {
+	    		for (j=31; j<33; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 17; i < 29; i++ ) {
+	    		for (j=31; j<33; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 31; i < 41; i++ ) {
+	    		for (j=31; j<33; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 43; i < 53; i++ ) {
+	    		for (j=31; j<33; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 55; i < 59; i++ ) {
+	    		for (j=31; j<33; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 11; i < 13; i++ ) {
+	    		for (j=33; j<35; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 31; i < 33; i++ ) {
+	    		for (j=33; j<39; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 43; i < 45; i++ ) {
+	    		for (j=33; j<39; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 55; i < 57; i++ ) {
+	    		for (j=33; j<35; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 3; i < 13; i++ ) {
+	    		for (j=35; j<37; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	    	for (i = 17; i < 21; i++ ) {
+	    		for (j=35; j<37; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	       	for (i = 23; i < 29; i++ ) {
+	    		for (j=35; j<37; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	       	for (i = 35; i < 43; i++ ) {
+	    		for (j=35; j<37; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	       	for (i = 47; i < 57; i++ ) {
+	    		for (j=35; j<37; j++) {
+	    			grid[i][j]=8;
+	    		}
+	    	}
+	      	for (i = 23; i < 25; i++ ) {
+		    	for (j=37; j<39; j++) {
+		    		grid[i][j]=8;
+		    	}
+		    }
+	
 	    	System.out.println(grid[i][j]);
 		
 	    	
