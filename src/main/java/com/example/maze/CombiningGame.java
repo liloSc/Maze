bddd5afc8c489d671ff3e43c10ccdaf6604ec4b9
lilalimpeldehseba <54209093+lilalimpeldehseba@users.noreply.github.com>
2022-10-25 20:07:20 +0200

    Enemy initialisation works with enter now.
    3 Enemies are now there, not much further specified in Enemy.class

--- src/main/java/com/example/maze/CombiningGame.java
+++ src/main/java/com/example/maze/CombiningGame.java
@@ -59,6 +59,7 @@ public class CombiningGame implements Initializable {
     
     
     public Player gamePlayer = new Player(null, 10);
+    private int numberOfEnemies;
 
     // ControllerCharacterSelection controllerCharacterSelection= new ControllerCharacterSelection();
 
@@ -140,11 +141,10 @@ public class CombiningGame implements Initializable {
         image = getCharacterImage();
         rectangleid.setFill(new ImagePattern(image));
         rectangleid.setVisible(false);
-
         instructionPane.toFront();
-
         gamelayoutgrid.setConstraints(rectangleid, 0, 1);
-        setRandomEnemiesToGame(5);
+       
+       // setRandomEnemiesToGame(5);
         doorClose.getLayoutBounds();
         xPosDoor = doorClose.getLayoutX();
         yPosDoor = doorClose.getLayoutY();
@@ -290,7 +290,6 @@ public class CombiningGame implements Initializable {
 
     }
 
-
     //fill the grid with a simple background
     private Node printGrid() {
 
@@ -306,12 +305,10 @@ public class CombiningGame implements Initializable {
                     backgroundView = new ImageView();
                     backgroundView.setImage(wall);
 
-
                     //System.out.println(grid[i][j]);
                 } else {
                     backgroundView = new ImageView();
                     backgroundView.setImage(path);
-
 
                     //System.out.println(grid[i][j]);
                 }
@@ -319,9 +316,7 @@ public class CombiningGame implements Initializable {
                 gamelayoutgrid.add(backgroundView, i, j);
                 backgroundView.toBack();
             }
-
         }
-
 
         return GPane;
     }
@@ -345,8 +340,6 @@ public class CombiningGame implements Initializable {
             isActive = true;
             if (event.getCode().equals(KeyCode.UP)) {
                 direction = Direction.UP;
-
-
                 if (gameLayout.isWall(i, j - 1) == false) {
                     gamelayoutgrid.setRowIndex(rectangleid, GridPane.getRowIndex(rectangleid) - 1);
                 }
@@ -376,7 +369,6 @@ public class CombiningGame implements Initializable {
             isPlayerOnDoorLilo(event);
             isPlayerNextToEnemy(event);
         }
-
     }
 
 
@@ -384,7 +376,6 @@ public class CombiningGame implements Initializable {
     void moveSquareKeyReleased(KeyEvent event) {
         if (KeyEvent.KEY_RELEASED.equals(event.getEventType())) {
             isActive = false;
-
         }
     }
 
@@ -403,8 +394,6 @@ public class CombiningGame implements Initializable {
         } else {
             //   System.out.println("Not on Door");
         }
-
-
     }
 
     private void isPlayerNextToEnemy(KeyEvent event) throws IOException {
@@ -467,11 +456,15 @@ public class CombiningGame implements Initializable {
     }
     
     
-    
+    // 3 off them are not right now needed, maybe later?
     public void initEnemy(Enemy ourEnemy1,Enemy ourEnemy2,Enemy ourEnemy3) {
 		this.GameClassEnemy1 = ourEnemy1;
 		this.GameClassEnemy2 = ourEnemy2;
 		this.GameClassEnemy3 = ourEnemy3;
+        System.out.println("Level in game is " + GameClassEnemy1.getLevelEnemy());
+        System.out.println("Level in game is " + GameClassEnemy2.getLevelEnemy());
+        System.out.println("Level in game is " + GameClassEnemy3.getLevelEnemy());
+        this.numberOfEnemies = Integer.parseInt(GameClassEnemy3.getLevelEnemy());
 	//	EnemyLabel1.setText(GameClassEnemy1.getPersonTraits());  // ourEnemy can be whatever
 	//	EnemyLabel2.setText(GameClassEnemy2.getPersonTraits());
 	//	EnemyLabel3.setText(GameClassEnemy3.getPersonTraits());
@@ -486,11 +479,10 @@ public class CombiningGame implements Initializable {
             rectangleid.requestFocus();
             movePlayerKeyPressed(event);
             moveSquareKeyReleased(event);
+            setRandomEnemiesToGame(numberOfEnemies);
 
         }
     }
-
-
 }
 
 
